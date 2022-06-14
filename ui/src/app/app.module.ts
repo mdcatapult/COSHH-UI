c671b084bb1f680db3cd256e08712b9a30cf770d
Nick Etherington <nick.etherington@md.catapult.org.uk>
2022-06-14 11:21:21 +0100

    Uses mat moment date adapter and UTC

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CoshhComponent } from './coshh/coshh.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {CoshhComponent} from './coshh/coshh.component';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatTableModule} from "@angular/material/table";
 import {MatIconModule} from "@angular/material/icon";
-import { AddChemicalComponent } from './add-chemical/add-chemical.component';
-import { AddChemicalDialogComponent } from './add-chemical-dialog/add-chemical-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule, MatOptionModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import {AddChemicalComponent} from './add-chemical/add-chemical.component';
+import {AddChemicalDialogComponent} from './add-chemical-dialog/add-chemical-dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule, MatOptionModule, MAT_DATE_LOCALE} from '@angular/material/core';
+import {MatMomentDateModule, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatTooltipModule} from '@angular/material/tooltip';
@@ -22,12 +23,12 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CoshhComponent,
-    AddChemicalComponent,
-    AddChemicalDialogComponent,
-  ],
+    declarations: [
+        AppComponent,
+        CoshhComponent,
+        AddChemicalComponent,
+        AddChemicalDialogComponent,
+    ],
     imports: [
         BrowserModule,
         HttpClientModule,
@@ -41,6 +42,7 @@ import {MatButtonModule} from "@angular/material/button";
         MatInputModule,
         MatDatepickerModule,
         MatNativeDateModule,
+        MatMomentDateModule,
         MatCheckboxModule,
         MatToolbarModule,
         MatTooltipModule,
@@ -48,9 +50,11 @@ import {MatButtonModule} from "@angular/material/button";
         MatAutocompleteModule,
         MatButtonModule,
     ],
-  providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
-  ],
-  bootstrap: [AppComponent]
+    providers: [
+        {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
+        {provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: {useUtc: true}}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
