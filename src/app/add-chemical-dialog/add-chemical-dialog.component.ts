b7ad4902211f4d90a156a8528edfdea1796fedba
Nick Etherington <nick.etherington@md.catapult.org.uk>
2022-06-14 12:37:47 +0100

    Uses moment in form

--- src/app/add-chemical-dialog/add-chemical-dialog.component.ts
+++ src/app/add-chemical-dialog/add-chemical-dialog.component.ts
@@ -1,8 +1,8 @@
 import {Component, Inject, Input} from '@angular/core';
-import {UntypedFormArray, UntypedFormControl, UntypedFormGroup, Validators} from '@angular/forms';
+import { FormGroup, Validators, FormControl, FormArray} from '@angular/forms';
 import {DateAdapter} from '@angular/material/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {Chemical} from '../coshh/types';
+import * as moment from 'moment';
 
 @Component({
     selector: 'app-add-chemical-dialog',
@@ -33,19 +33,19 @@ export class AddChemicalDialogComponent {
 
     selectedHazardCategories: string[] = []
 
-    form = new UntypedFormGroup({
-        casNumber: new UntypedFormControl('', [Validators.pattern('\\b[1-9]{1}\\d{1,5}-\\d{2}-\\d\\b'), Validators.required]),
-        name: new UntypedFormControl('', Validators.required),
-        photoPath: new UntypedFormControl('', Validators.required),
-        matterState: new UntypedFormControl('', Validators.required),
-        quantity: new UntypedFormControl('', Validators.required),
-        added: new UntypedFormControl(new Date(), Validators.required),
-        expiry: new UntypedFormControl(new Date(), Validators.required),
-        safetyDataSheet: new UntypedFormControl('', Validators.required),
-        coshhLink: new UntypedFormControl(''),
-        storageTemp: new UntypedFormControl('', Validators.required),
-        location: new UntypedFormControl(''),
-        hazards: new UntypedFormArray(this.hazardCategories.map(() => new UntypedFormControl(false)), Validators.required)
+    form = new FormGroup({
+        casNumber: new FormControl('', [Validators.pattern('\\b[1-9]{1}\\d{1,5}-\\d{2}-\\d\\b'), Validators.required]),
+        name: new FormControl('', Validators.required),
+        photoPath: new FormControl('', Validators.required),
+        matterState: new FormControl('', Validators.required),
+        quantity: new FormControl('', Validators.required),
+        added: new FormControl(moment(new Date(), "DD-MM-YYY"), Validators.required),
+        expiry: new FormControl(moment(new Date(), "DD-MM-YYY"), Validators.required),
+        safetyDataSheet: new FormControl('', Validators.required),
+        coshhLink: new FormControl(''),
+        storageTemp: new FormControl('', Validators.required),
+        location: new FormControl(''),
+        hazards: new FormArray(this.hazardCategories.map(() => new FormControl('')), Validators.required)
     });
 
     onClose(): void {
@@ -61,7 +61,6 @@ export class AddChemicalDialogComponent {
         } else {
             this.selectedHazardCategories.splice(this.selectedHazardCategories.indexOf(changedCategory))
         }
-        console.log(this.selectedHazardCategories)
     }
 
 }
