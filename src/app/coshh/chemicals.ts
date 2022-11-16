5aef810702d4761002290084e2f870a057d105be
Roman Ma <roman.ma@md.catapult.org.uk>
2022-11-16 14:43:44 +0000

    implement search by chem no and change its implementation to ensure name/chem no filtering on changing other filters

--- src/app/coshh/chemicals.ts
+++ src/app/coshh/chemicals.ts
@@ -4,7 +4,8 @@ import { Chemical, Expiry } from "./types"
 export class Chemicals {
     private chemicals: Chemical[] = []
 
-    get = (includeArchived: boolean, cupboard: string, hazardCategory: string, lab: string, expiry: Expiry): Chemical[] => {
+    get = (includeArchived: boolean, cupboard: string, hazardCategory: string, lab: string, expiry: Expiry, searchStr: string): Chemical[] => {
+        var searchLower = searchStr.toLowerCase()
         return this.chemicals
             .filter(chemical => includeArchived || !chemical.isArchived)
             .filter(chemical => cupboard === 'All' || chemical.cupboard === cupboard)
@@ -12,6 +13,7 @@ export class Chemicals {
                 chemical.hazards?.map(hazard => hazard.toString()).includes(hazardCategory))
             .filter(chemical => lab === 'All' || chemical.location === lab)
             .filter(chemical => Chemicals.filterExpiryDate(chemical, expiry))
+            .filter(chemical => chemical.name.toLowerCase().includes(searchLower) || chemical.chemicalNumber.toLowerCase().includes(searchLower))
             .sort((a, b) => {
                 if (a.name < b.name) {
                     return -1
@@ -25,9 +27,12 @@ export class Chemicals {
     add = (chemical: Chemical) => this.chemicals.push(chemical)
     set = (chemicals: Chemical[]) => this.chemicals = chemicals
     getNames = (chemicals: Chemical[], search: string): string[] => {
+        var searchLower = search.toLowerCase()
         return chemicals
-            .filter(chemical => chemical.name.toLowerCase().includes(search.toLowerCase()))
-            .map(chemical => chemical.name)
+            .flatMap(chemical => [chemical.name, chemical.chemicalNumber])
+            .filter(phrase => phrase.toLowerCase().includes(searchLower))
+            .sort()
+            .filter((item, pos, array) => !pos || item != array[pos - 1])  // deduplication
     }
 
     update = (chemical: Chemical) => {
