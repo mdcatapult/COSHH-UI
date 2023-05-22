922187bac43635911abbb65aea7b3d07049115a1
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2023-05-22 16:17:47 +0100

    Use different build stage & Dockerfile for prod v dev

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -1,14 +1,9 @@
 const { writeFile } = require('fs');
-const { argv } = require('yargs');
 
 // read environment variables from .env file
 require('dotenv').config();
 
-// read the command line arguments passed with yargs
 const isProduction = process.env["DEPLOYMENT_ENV"] === 'prod'
-const targetPath = isProduction
-    ? `./src/environments/environment.prod.ts`
-    : `./src/environments/environment.ts`;
 
 // we have access to our environment variables
 // in the process.env object thanks to dotenv
@@ -28,10 +23,8 @@ export const environment = {
 `;
 
 // write the content to the respective file
-writeFile(targetPath, environmentFileContent, function (err: any) {
+writeFile(`./src/environments/environment.ts`, environmentFileContent, function (err: any) {
     if (err) {
         console.log(err);
     }
-
-    console.log(`Wrote variables to ${targetPath}`);
 });
