7d0f2c6dc5800b16f82afd8d8a0faa39748ca1aa
Nick Etherington <nick.etherington@md.catapult.org.uk>
2023-05-24 15:36:36 +0100

    Only call moment in DateTimeFormatPipe if there is an expiry date.

--- src/app/utility/pipes/my-datetime-format.pipe.ts
+++ src/app/utility/pipes/my-datetime-format.pipe.ts
@@ -6,6 +6,6 @@ import * as moment from 'moment';
 })
 export class DateTimeFormatPipe implements PipeTransform {
   transform(value: string): string {
-    return moment(value).format('DD-MM-YYYY');
+    return value ? moment(value).format('DD-MM-YYYY') : '';
   }
 }
