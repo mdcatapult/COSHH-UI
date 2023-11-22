e8b08e6a36e5f0323136b8eb420cdcdf8aa92f0e
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2023-11-22 11:14:52 +0000

    Final updates from old gitlab

--- src/app/utility/utilities.ts
+++ src/app/utility/utilities.ts
@@ -1,60 +1,69 @@
-import {AbstractControl, UntypedFormControl, ValidationErrors, ValidatorFn} from "@angular/forms";
-import {map, Observable} from "rxjs";
-import {Columns, SheetData} from "write-excel-file";
+import { AbstractControl, UntypedFormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { map, Observable } from 'rxjs';
 
-import {DateTimeFormatPipe} from './pipes/my-datetime-format.pipe';
-import {Chemical} from "../coshh/types";
+import { Chemical } from '../coshh/types';
+import { Columns, SheetData } from 'write-excel-file';
+import { DateTimeFormatPipe } from './pipes/my-datetime-format.pipe';
 
 export function getAutocompleteObservable(formControl: UntypedFormControl, data: string[]): Observable<string[]> {
     return formControl.valueChanges.pipe(
-        map(value => {
+        map((value) => {
                 return data
                     .filter((option: string) => option.toLowerCase()
-                        .includes(value.toLowerCase()))
+                        .includes(value.toLowerCase()));
             }
         )
-    )
+    );
 }
 
-export function createExcelData(columnNames: String[], chemicals: Chemical[]) {
-    const HEADER_ROW: SheetData = [columnNames.map(columnName => {
+export function createExcelData(columnNames: string[], chemicals: Chemical[]) {
+    const HEADER_ROW: SheetData = [columnNames.map((columnName) => {
 
-        return {value: columnName, fontSize: 15, fontWeight: 'bold', align: 'center'}
-    })]
-    const chemicalsToSave: SheetData = chemicals.map(chemical => {
+        return { value: columnName, fontSize: 15, fontWeight: 'bold', align: 'center' };
+    })];
+
+    const chemicalsToSave: SheetData = chemicals.map((chemical) => {
         const row: SheetData = [[
-            {type: String, value: chemical.name || '', wrap: true},
-            {type: String, value: chemical.quantity || ''},
-            {type: String, value: chemical.location || ''},
-            {type: String, value: chemical.safetyDataSheet || '', wrap: true},
-            chemical.added ? {type: Date, value: new Date(chemical.added.toString()), format: 'dd/mm/yyyy'} : {type: String, value: ''},
-            chemical.expiry ? {type: Date, value: new Date(chemical.expiry.toString()), format: 'dd/mm/yyyy'} : {type: String, value: ''},
-            {type: String, wrap: true}
-        ]]
+            { type: String, value: chemical.name || '', wrap: true },
+            { type: String, value: chemical.quantity || '' },
+            { type: String, value: chemical.casNumber || '' },
+            { type: String, value: chemical.matterState || '' },
+            { type: String, value: chemical.location || '' },
+            { type: String, value: chemical.cupboard || '' },
+            { type: String, value: chemical.safetyDataSheet || '', wrap: true },
+            chemical.added ? { type: Date, value: new Date(chemical.added.toString()), format: 'dd/mm/yyyy' } : { type: String, value: '' },
+            chemical.expiry ? { type: Date, value: new Date(chemical.expiry.toString()), format: 'dd/mm/yyyy' } : { type: String, value: '' },
+            { type: String, wrap: true }
+        ]];
 
         // this looks weird but it's the only way to get the types to play ball
-        return row[0]
-    })
-    const data: SheetData = [...HEADER_ROW, ...chemicalsToSave]
-    const columnOptions: Columns = [{width: 30}, {}, {width: 15}, {width: 50}, {width: 12}, {width: 12}, {width: 50}]
+        return row[0];
+    });
 
-    return {data, columnOptions}
+    const data: SheetData = [...HEADER_ROW, ...chemicalsToSave];
+
+    const columnOptions: Columns = [{ width: 30 }, {}, { width: 15 }, { width: 15 }, { width: 15 }, { width: 15 }, { width: 50 }, { width: 12 }, { width: 12 }, { width: 50 }];
+
+    return { data, columnOptions };
 }
 
 export function createPDFData(chemicals: Chemical[]) {
-    const dateTimePipe = new DateTimeFormatPipe()
+    const dateTimePipe = new DateTimeFormatPipe();
 
-    return chemicals.map(chemical => {
+    return chemicals.map((chemical) => {
 
         return {
             'Name': chemical.name || '',
             'Quantity': chemical.quantity || '',
+            'CAS No.': chemical.casNumber || '',
+            'State': chemical.matterState || '',
             'Location': chemical.location || '',
+            'Cupboard': chemical.cupboard || '',
             'Safety data sheet': chemical.safetyDataSheet || '',
             'Added': chemical.added ? dateTimePipe.transform(chemical.added.toString()) : '',
             'Expiry': chemical.expiry ? dateTimePipe.transform(chemical.expiry.toString()) : ''
-        }
-    })
+        };
+    });
 }
 
 /**
@@ -70,7 +79,7 @@ export function isValidHttpUrl(urlString: string): boolean {
         return false;
     }
 
-    return url.protocol === "http:" || url.protocol === "https:";
+    return url.protocol === 'http:' || url.protocol === 'https:';
 }
 
 export function urlValidator(): ValidatorFn {
@@ -78,11 +87,11 @@ export function urlValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
         if (!control.value) {
 
-            return null
+            return null;
         }
 
-        return isValidHttpUrl(control.value) ? null : {invalidURL: control.value};
-    }
+        return isValidHttpUrl(control.value) ? null : { invalidURL: control.value };
+    };
 }
 
 /**
@@ -90,6 +99,7 @@ export function urlValidator(): ValidatorFn {
  * @param things
  */
 export function checkDuplicates(things: string[]): string[] {
-    let duplicatesRemoved: string[] = Array.from(new Set(things.map(e => e.toLowerCase().trim())));
-    return duplicatesRemoved
+    const duplicatesRemoved: string[] = Array.from(new Set(things.map((e) => e.toLowerCase().trim())));
+
+    return duplicatesRemoved;
 }
