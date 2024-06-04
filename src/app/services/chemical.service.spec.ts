f6a906b0282a1580c49dad316f2b1d68feadc630
Nick Etherington <nick.etherington@md.catapult.org.uk>
2024-06-04 13:11:03 +0100

    Linting.

--- src/app/services/chemical.service.spec.ts
+++ src/app/services/chemical.service.spec.ts
@@ -36,6 +36,7 @@ describe('ChemicalService', () => {
 
     beforeEach(() => {
         const httpClientSpyObj = jasmine.createSpyObj('HttpClient', ['post', 'put']);
+
         TestBed.configureTestingModule({
             imports: [
                 // Fake Auth0 details for testing purposes
@@ -68,74 +69,90 @@ describe('ChemicalService', () => {
     });
 
     it('should be created', () => {
+
         expect(chemicalService).toBeTruthy();
     });
 
     it('should get chemicals from the data service on initialisation', () => {
+
         expect(dataService.getChemicals).toHaveBeenCalled();
     });
 
     it('should set chemicals in state on initialisation', () => {
+
         expect(chemicalService.getAllChemicals()).toEqual(allChemicals);
     });
 
     it('should filter chemicals when lab filter is applied', () => {
         chemicalService.labFilterControl.setValue('Lab 1');
+
         expect(chemicalService.getFilteredChemicals()).toEqual([chemicalOne]);
     });
 
     it('should filter chemicals when cupboard filter is applied', () => {
         chemicalService.cupboardFilterControl.setValue('cupboard 3');
+
         expect(chemicalService.getFilteredChemicals()).toEqual([chemicalThree]);
     });
 
     it('filteredChemicals should not include archived chemicals by default', () => {
+
         expect(chemicalService.getFilteredChemicals().includes(archivedChemical)).toEqual(false);
     });
 
     it('filtered chemicals should include archived chemicals when archive filter is toggled on', () => {
         chemicalService.toggleArchiveControl.setValue(true);
+
         expect(chemicalService.getFilteredChemicals().includes(archivedChemical)).toEqual(true);
     });
 
     it('should filter chemicals when hazard filter is applied', () => {
         chemicalService.hazardFilterControl.setValue('Explosive');
+
         expect(chemicalService.getFilteredChemicals()).toEqual([chemicalOne]);
     });
 
     it('should filter chemicals when expiry filter is applied', () => {
         chemicalService.expiryFilterControl.setValue('< 30 Days');
+
         expect(chemicalService.getFilteredChemicals()).toEqual([chemicalTwo]);
     });
 
     it('should filter chemicals with search string', () => {
         chemicalService.nameOrNumberSearchControl.setValue('1');
+
         expect(chemicalService.getFilteredChemicals()).toEqual([chemicalOne]);
     });
 
     it('should get labs from the data service on initialisation', () => {
+
         expect(dataService.getLabs).toHaveBeenCalled();
     });
 
     it('should set labs in state on initialisation and add \'All\' to the array', () => {
+
         expect(chemicalService.labFilterValues.sort()).toEqual(['All', 'Lab 1', 'Lab 2', 'Lab 3'].sort());
     });
 
     it('should get cupboards from the data service on initialisation', () => {
+
         expect(dataService.getCupboards).toHaveBeenCalled();
     });
 
     it('should lowercase and set cupboards in state on initialisation and add \'All\' to the array', () => {
+
         expect(chemicalService.cupboardFilterValues.sort()).toEqual(['All', 'cupboard 1', 'cupboard 2', 'cupboard 3'].sort());
     });
 
     it('should call getCupboardsForLab when a lab is selected', () => {
         chemicalService.labFilterControl.setValue('Lab 1');
+
         expect(dataService.getCupboardsForLab).toHaveBeenCalledWith('Lab 1');
     });
 
     it('should update cupboard filter options when a lab is selected', () => {
         chemicalService.labFilterControl.setValue('Lab 1');
+
         expect(chemicalService.cupboardFilterValues.sort()).toEqual(['All', 'cupboard 2'].sort());
     });
 
@@ -146,6 +163,7 @@ describe('ChemicalService', () => {
             chemicalService.onChemicalAdded(newChemical);
             spyOn(chemicalService, 'setAllChemicals').and.callThrough();
             tick();
+
             expect(chemicalService.getAllChemicals().includes(newChemical)).toEqual(true);
         }));
 
@@ -154,6 +172,7 @@ describe('ChemicalService', () => {
             chemicalService.onChemicalAdded(newChemical);
             spyOn(chemicalService, 'setFilteredChemicals').and.callThrough();
             tick();
+
             expect(chemicalService.getFilteredChemicals().includes(newChemical)).toEqual(true);
         }));
 
@@ -163,34 +182,41 @@ describe('ChemicalService', () => {
             chemicalService.onChemicalAdded(newChemical);
             spyOn(chemicalService, 'setFilteredChemicals').and.callThrough();
             tick();
+
             expect(chemicalService.getFilteredChemicals().includes(newChemical)).toEqual(false);
         }));
 
         it('should get hazard list for the new chemical', fakeAsync(() => {
             const hazardListSpy = spyOn(hazardService, 'getHazardListForChemical').and.callThrough();
+
             httpClientSpy.post.and.returnValue(asyncData(newChemical));
             chemicalService.onChemicalAdded(newChemical);
             tick();
+
             expect(hazardListSpy).toHaveBeenCalledWith(newChemical);
         }));
 
         it('should get expiryColour for the new chemical', fakeAsync(() => {
             const expiryColourSpy = spyOn(expiryService, 'getExpiryColour').and.callThrough();
+
             httpClientSpy.post.and.returnValue(asyncData(newChemical));
             chemicalService.onChemicalAdded(newChemical);
             tick();
+
             expect(expiryColourSpy).toHaveBeenCalledWith(newChemical);
         }));
 
         it('should format the cupboard name for the new chemical', () => {
             httpClientSpy.post.and.returnValue(asyncData(newChemical));
             chemicalService.onChemicalAdded(newChemical);
+
             expect(newChemical.cupboard).toEqual('cupboard 99');
         });
 
         it('should make a POST request to the API to add the new chemical', () => {
             httpClientSpy.post.and.returnValue(asyncData(newChemical));
             chemicalService.onChemicalAdded(newChemical);
+
             expect(httpClientSpy.post).toHaveBeenCalledTimes(1);
             expect(httpClientSpy.post).toHaveBeenCalledWith(`${environment.backendUrl}/chemical`, newChemical);
         });
@@ -201,6 +227,7 @@ describe('ChemicalService', () => {
         it('should make 2 PUT requests to the API to update the chemical and hazards', () => {
             httpClientSpy.put.and.returnValue(asyncData([]));
             chemicalService.onChemicalEdited(updatedChemical);
+
             expect(httpClientSpy.put).toHaveBeenCalledTimes(2);
             expect(httpClientSpy.put).toHaveBeenCalledWith(`${environment.backendUrl}/chemical`, updatedChemical);
             expect(httpClientSpy.put).toHaveBeenCalledWith(`${environment.backendUrl}/hazards`, updatedChemical);
@@ -208,17 +235,21 @@ describe('ChemicalService', () => {
 
         it('should get hazard list for updated chemical', fakeAsync(() => {
             const hazardListSpy = spyOn(hazardService, 'getHazardListForChemical').and.callThrough();
+
             httpClientSpy.put.and.returnValue(asyncData([]));
             chemicalService.onChemicalEdited(updatedChemical);
             tick();
+
             expect(hazardListSpy).toHaveBeenCalledWith(updatedChemical);
         }));
 
         it('should get expiryColour for updated chemical', fakeAsync(() => {
             const expiryColourSpy = spyOn(expiryService, 'getExpiryColour').and.callThrough();
+
             httpClientSpy.put.and.returnValue(asyncData([]));
             chemicalService.onChemicalEdited(updatedChemical);
             tick();
+
             expect(expiryColourSpy).toHaveBeenCalledWith(updatedChemical);
         }));
 
@@ -226,14 +257,17 @@ describe('ChemicalService', () => {
             spyOn(chemicalService, 'updateChemical').and.callThrough();
             httpClientSpy.put.and.returnValue(asyncData(updatedChemical));
             chemicalService.onChemicalEdited(updatedChemical);
+
             expect(chemicalService.updateChemical).toHaveBeenCalled();
         });
 
         it('should call updateHazards', fakeAsync(() => {
             const updateHazardsSpy = spyOn(hazardService, 'updateHazards').and.callThrough();
+
             httpClientSpy.put.and.returnValue(asyncData([]));
             chemicalService.onChemicalEdited(updatedChemical);
             tick();
+
             expect(updateHazardsSpy).toHaveBeenCalledWith(updatedChemical);
         }));
 
@@ -241,6 +275,7 @@ describe('ChemicalService', () => {
             spyOn(chemicalService, 'update').and.callThrough();
             httpClientSpy.put.and.returnValue(asyncData(updatedChemical));
             chemicalService.onChemicalEdited(updatedChemical);
+
             expect(chemicalService.update).toHaveBeenCalled();
         });
     });
@@ -251,6 +286,7 @@ describe('ChemicalService', () => {
             httpClientSpy.put.and.returnValue(asyncData(updatedChemical));
             chemicalService.loggedInUser = 'test.user@md.catapult.org.uk';
             chemicalService.updateChemical(updatedChemical).subscribe((chem: Chemical) => {
+
                 expect(chem.lastUpdatedBy).toEqual('test.user@md.catapult.org.uk');
             });
         });
@@ -262,12 +298,14 @@ describe('ChemicalService', () => {
         it('should update allChemicals', () => {
             spyOn(chemicalService, 'setAllChemicals').and.callThrough();
             chemicalService.update(newChemical);
+
             expect(chemicalService.setAllChemicals).toHaveBeenCalled();
         });
 
         it('should update filteredChemicals', () => {
             spyOn(chemicalService, 'setFilteredChemicals').and.callThrough();
             chemicalService.update(newChemical);
+
             expect(chemicalService.setFilteredChemicals).toHaveBeenCalled();
         });
     });
@@ -276,23 +314,27 @@ describe('ChemicalService', () => {
 
         it('should archive a chemical which is not already archived', () => {
             chemicalService.archive(chemicalTwo);
+
             expect(chemicalTwo.isArchived).toEqual(true);
         });
 
         it('should unarchive a chemical which is already archived', () => {
             chemicalService.archive(chemicalTwo);
+
             expect(chemicalTwo.isArchived).toEqual(false);
         });
 
         it('should call updateChemical', () => {
             spyOn(chemicalService, 'updateChemical').and.callThrough();
             chemicalService.archive(chemicalThree);
+
             expect(chemicalService.updateChemical).toHaveBeenCalled();
         });
 
         it('should call update', () => {
             spyOn(chemicalService, 'update').and.callThrough();
             chemicalService.archive(chemicalFour);
+
             expect(chemicalService.update).toHaveBeenCalled();
         });
     });
@@ -302,20 +344,24 @@ describe('ChemicalService', () => {
         it('should return a list of all cupboards if the lab is All', () => {
             chemicalService.labFilterControl.setValue('All');
             chemicalService.refreshCupboardsFilterList();
+
             expect(chemicalService.cupboardFilterValues.sort()).toEqual(['All', 'cupboard 1', 'cupboard 2', 'cupboard 3'].sort());
         });
 
         it('list of cupboards should not contain duplicates', () => {
+
             expect(chemicalService.cupboardFilterValues.sort()).toEqual(['All', 'cupboard 1', 'cupboard 2', 'cupboard 3'].sort());
         });
 
         it('list of cupboards should contain All', () => {
+
             expect(chemicalService.cupboardFilterValues).toContain('All');
         });
 
         it('should return a filtered list of cupboards when a lab is selected', () => {
             chemicalService.labFilterControl.setValue('Lab 1');
             chemicalService.refreshCupboardsFilterList();
+
             expect(chemicalService.cupboardFilterValues.sort()).toEqual(['All', 'cupboard 2'].sort());
         });
     });
@@ -324,6 +370,7 @@ describe('ChemicalService', () => {
 
         it('should sort the filtered chemicals by name', () => {
             const actual = chemicalService.filterChemicals(true, 'All', 'All', 'All', 'Any', '', '');
+
             const expected = allChemicals.sort((a, b) => {
                 if (a.name < b.name) {
 
